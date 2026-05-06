cc1ebae8a84b508320fddbb3fd8d87fb80e57d14
Alexander Schilling <mail@alexanderschilling.de>
2026-05-06 11:20:26 +0200

    Add aggregation script to compute the mean and standard deviation of multiple runs with different seeds

---
+++ src/aggregate_results.py
@@ -0,0 +1,110 @@
+import argparse
+import os
+import glob
+import json
+from typing import List
+
+import numpy as np
+
+
+def merge_dicts(dicts):
+    result = {}
+    for k in dicts[0].keys():
+        values = [d[k] for d in dicts]
+        if isinstance(dicts[0][k], dict):
+            result[k] = merge_dicts(values)
+        else:
+            result[k] = values
+    return result
+
+
+def aggregate_dict(d):
+    result = {}
+    for k in d.keys():
+        if isinstance(d[k], dict):
+            result[k] = aggregate_dict(d[k])
+        else:
+            result[f"{k}_mean"] = np.mean(d[k])
+            result[f"{k}_std"] = np.std(d[k])
+    return result
+
+
+def aggregate_results(directories: List[str]):
+    result_dicts = []
+    for pattern in directories:
+        for path in glob.glob(pattern):
+            with open(os.path.join(path, "results.json")) as f:
+                result_dicts.append(json.load(f))
+    return aggregate_dict(merge_dicts(result_dicts))
+
+
+def build_error_table(res):
+    s_r_mae_mean = res["single_task_r"]["mae_0_mean"]
+    s_r_mae_std = res["single_task_r"]["mae_0_std"]
+    s_r_rmse_mean = res["single_task_r"]["rmse_0_mean"]
+    s_r_rmse_std = res["single_task_r"]["rmse_0_std"]
+    s_z_mae_mean = res["single_task_z"]["mae_0_mean"]
+    s_z_mae_std = res["single_task_z"]["mae_0_std"]
+    s_z_rmse_mean = res["single_task_z"]["rmse_0_mean"]
+    s_z_rmse_std = res["single_task_z"]["rmse_0_std"]
+    m_r_mae_mean = res["multitask_weighted_sum"]["mae_0_mean"]
+    m_r_mae_std = res["multitask_weighted_sum"]["mae_0_std"]
+    m_r_rmse_mean = res["multitask_weighted_sum"]["rmse_0_mean"]
+    m_r_rmse_std = res["multitask_weighted_sum"]["rmse_0_std"]
+    m_z_mae_mean = res["multitask_weighted_sum"]["mae_1_mean"]
+    m_z_mae_std = res["multitask_weighted_sum"]["mae_1_std"]
+    m_z_rmse_mean = res["multitask_weighted_sum"]["rmse_1_mean"]
+    m_z_rmse_std = res["multitask_weighted_sum"]["rmse_1_std"]
+    h_r_mae_mean = res["multitask_homoscedastic"]["mae_0_mean"]
+    h_r_mae_std = res["multitask_homoscedastic"]["mae_0_std"]
+    h_r_rmse_mean = res["multitask_homoscedastic"]["rmse_0_mean"]
+    h_r_rmse_std = res["multitask_homoscedastic"]["rmse_0_std"]
+    h_z_mae_mean = res["multitask_homoscedastic"]["mae_1_mean"]
+    h_z_mae_std = res["multitask_homoscedastic"]["mae_1_std"]
+    h_z_rmse_mean = res["multitask_homoscedastic"]["rmse_1_mean"]
+    h_z_rmse_std = res["multitask_homoscedastic"]["rmse_1_std"]
+    table_code = f"        Single task " \
+                 f"& ${s_r_mae_mean:.3f} \\pm {s_r_mae_std:.3f}$ " \
+                 f"& ${s_z_mae_mean:.3f} \\pm {s_z_mae_std:.3f}$ " \
+                 f"& ${s_r_rmse_mean:.3f} \\pm {s_r_rmse_std:.3f}$ " \
+                 f"& ${s_z_rmse_mean:.3f} \\pm {s_z_rmse_std:.3f}$\\\\\n" \
+                 f"        Weighted sum " \
+                 f"& ${m_r_mae_mean:.3f} \\pm {m_r_mae_std:.3f}$ " \
+                 f"& ${m_z_mae_mean:.3f} \\pm {m_z_mae_std:.3f}$ " \
+                 f"& ${m_r_rmse_mean:.3f} \\pm {m_r_rmse_std:.3f}$ " \
+                 f"& ${m_z_rmse_mean:.3f} \\pm {m_z_rmse_std:.3f}$\\\\\n" \
+                 f"        Homoscedastic " \
+                 f"& ${h_r_mae_mean:.3f} \\pm {h_r_mae_std:.3f}$ " \
+                 f"& ${h_z_mae_mean:.3f} \\pm {h_z_mae_std:.3f}$ " \
+                 f"& ${h_r_rmse_mean:.3f} \\pm {h_r_rmse_std:.3f}$ " \
+                 f"& ${h_z_rmse_mean:.3f} \\pm {h_z_rmse_std:.3f}$\\\\\n"
+    print("MAE/RMSE table")
+    print(table_code)
+
+
+def build_transfer_error_table(res):
+    m_r_mae_mean = res["multitask_weighted_sum"]["other"]["mae_0_mean"]
+    m_r_mae_std = res["multitask_weighted_sum"]["other"]["mae_0_std"]
+    m_z_mae_mean = res["multitask_weighted_sum"]["other"]["mae_1_mean"]
+    m_z_mae_std = res["multitask_weighted_sum"]["other"]["mae_1_std"]
+    m_r_rmse_mean = res["multitask_weighted_sum"]["other"]["rmse_0_mean"]
+    m_r_rmse_std = res["multitask_weighted_sum"]["other"]["rmse_0_std"]
+    m_z_rmse_mean = res["multitask_weighted_sum"]["other"]["rmse_1_mean"]
+    m_z_rmse_std = res["multitask_weighted_sum"]["other"]["rmse_1_std"]
+    table_code = f"${m_r_mae_mean:.3f} \\pm {m_r_mae_std:.3f}$ " \
+                 f"& ${m_z_mae_mean:.3f} \\pm {m_z_mae_std:.3f}$ " \
+                 f"& ${m_r_rmse_mean:.3f} \\pm {m_r_rmse_std:.3f}$ " \
+                 f"& ${m_z_rmse_mean:.3f} \\pm {m_z_rmse_std:.3f}$\\\\"
+    print("MAE/RMSE values on 'other' data")
+    print(table_code)
+    print("Rejection rate:", res["multitask_weighted_sum"]["other"]["rr_1_calib_mean"], "+-", res["multitask_weighted_sum"]["other"]["rr_1_calib_std"])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directories", metavar="directories", type=str, nargs="+")
+    args = parser.parse_args()
+
+    results = aggregate_results(args.directories)
+    build_error_table(results)
+    build_transfer_error_table(results)
